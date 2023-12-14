915c191ab3c0995f303e1630a60c42a923bd7ac9
obrucheoghene <wilfredcloudspace@gmail.com>
2023-12-14 22:43:37 +0100

    feat: add signin screen

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
 import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import AuthOptions from './screens/auth/AuthOptions';
 import Signup from './screens/auth/Signup';
+import Signin from './screens/auth/Signin';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar backgroundColor='#181a21' barStyle='light-content'/>
       {/* <AuthOptions/> */}
-      <Signup/>
+      {/* <Signup/> */}
+      <Signin/>
     </View>
   );
 }
---
+++ screens/auth/Signin.tsx
@@ -0,0 +1,78 @@
+import { Image, Pressable, StyleSheet, Text, TextInput, View } from "react-native"
+import { FontAwesome, MaterialIcons } from '@expo/vector-icons';
+
+
+import { buttonStyles, inputStyles } from "../../styles/styles";
+import Divider from "../../components/Divider";
+import AuthIcons from "../../components/AuthIcons";
+import ActionText from "./ActionText";
+
+const Signin = () => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Login to Your Account</Text>
+
+
+
+            <View style={inputStyles.container}>
+                <FontAwesome name='envelope' size={18} color="white" />
+                <TextInput placeholder='Email' style={inputStyles.input}
+                    placeholderTextColor={'#ccc'} />
+            </View>
+
+            <View style={inputStyles.container}>
+                <FontAwesome name='lock' size={22} color="white" />
+
+                <TextInput placeholder='Password' style={inputStyles.input}
+                    placeholderTextColor={'#ccc'} />
+                <FontAwesome name='eye' size={22} color="white" />
+            </View>
+
+            <Pressable style={buttonStyles.blueContainer}>
+                <Text style={styles.text}>Sign in</Text>
+            </Pressable>
+
+            <Text style={styles.forgotPasswordText}>Forgot your Password?</Text>
+
+            <Divider text="or continue with" />
+
+            <AuthIcons/>
+
+            <ActionText question="Already have an account?" actionText="Sign in"/>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        rowGap: 15,
+        padding: 20,
+    },
+    image: {
+        width: 100,
+        height: 100,
+    },
+    title: {
+        color: '#fff',
+        fontSize: 30,
+    },
+    text: {
+        color: '#fff',
+        fontSize: 18
+    },
+
+    icon: {
+        width: 30,
+        height: 30
+    },
+    forgotPasswordText: {
+        color: '#3062c8',
+        fontSize: 18
+    }
+})
+
+
+export default Signin
