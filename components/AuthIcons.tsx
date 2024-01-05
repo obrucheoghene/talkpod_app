56be790f0af54b9636dfe16de5597a0810582ac8
obrucheoghene <wilfredcloudspace@gmail.com>
2024-01-05 09:13:53 +0100

    chore: use app button component

--- components/AuthIcons.tsx
+++ components/AuthIcons.tsx
@@ -1,23 +1,23 @@
 import React from 'react'
-import { Image, Pressable, StyleSheet, View } from 'react-native'
+import { Image, StyleSheet, View } from 'react-native'
 
 import kingschat from '../assets/kingschat-logo.png'
 import apple from '../assets/apple-logo.png'
 import google from '../assets/google-logo.png'
-import { buttonStyles } from '../styles/styles'
+import AppButton from './AppButton'
 
 const AuthIcons = () => {
   return (
     <View style={styles.container}>
-    <Pressable style={[buttonStyles.darkContainer, styles.iconButton]}>
+    <AppButton style={ styles.iconButton}>
         <Image source={kingschat} style={styles.icon} />
-    </Pressable>
-    <Pressable style={[buttonStyles.darkContainer, styles.iconButton]}>
+    </AppButton>
+    <AppButton style={ styles.iconButton}>
         <Image source={google} style={styles.icon} />
-    </Pressable>
-    <Pressable style={[buttonStyles.darkContainer, styles.iconButton]}>
+    </AppButton>
+    <AppButton style={styles.iconButton}>
         <Image source={apple} style={styles.icon} />
-    </Pressable>
+    </AppButton>
 </View>
   )
 }
