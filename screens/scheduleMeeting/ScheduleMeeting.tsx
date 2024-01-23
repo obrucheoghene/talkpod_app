59b05486fb62fb7ef615dcf5fc82894e7bf53790
obrucheoghene <wilfredcloudspace@gmail.com>
2024-01-23 16:12:06 +0100

    feat: design schedule meeting screen

--- screens/scheduleMeeting/ScheduleMeeting.tsx
+++ screens/scheduleMeeting/ScheduleMeeting.tsx
@@ -11,6 +11,7 @@ import Spacer from "../../components/Spacer";
 import { AppFonts } from "../../types/interfaces";
 import Divider from "../../components/Divider";
 import AppButton from "../../components/AppButton";
+import RightChevron from '../../assets/svg/white-right-chevron.svg'
 
 type ScreenProps = NativeStackScreenProps<ScreenParams, 'ScheduleMeeting'>;
 
@@ -21,25 +22,64 @@ const ScheduleMeeting = ({ navigation }: ScreenProps) => {
     <ScreenContainer  >
       <Spacer height={2} />
       <View style={{ rowGap: theme.sizes.b, flex: 1 }}>
-        <AppText style={styles.label} font={AppFonts["Urbanist-Medium"]}>Meeting Id</AppText>
+        <AppText style={styles.label} font={AppFonts["Urbanist-Medium"]}>Meeting Title</AppText>
         <ViewContainer>
-          <AppInput placeholder="Email" />
+          <AppInput placeholder="Meeting title" />
         </ViewContainer>
 
-        <AppText style={styles.label} font={AppFonts["Urbanist-Medium"]}>Display Name</AppText>
-        <ViewContainer>
-          <AppInput placeholder="Email" />
-        </ViewContainer>
         <Divider />
+        {/* Date */}
+        <View style={styles.option}>
+          <AppText style={{ flex: 1 }} font={AppFonts['Urbanist-Medium']} >Date </AppText>
+          <View style={styles.option}>
+            <AppText font={AppFonts['Urbanist-Medium']} >12/24/2024 </AppText>
+            <RightChevron width={15} height={15} />
+          </View>
+        </View>
 
-        <AppText font={AppFonts['Urbanist-Medium']} >Join Options</AppText>
+        {/* From */}
+        <View style={styles.option}>
+          <AppText style={{ flex: 1 }} font={AppFonts['Urbanist-Medium']} >From </AppText>
+          <View style={styles.option}>
+            <AppText font={AppFonts['Urbanist-Medium']} >14:00 </AppText>
+            <RightChevron width={15} height={15} />
+          </View>
+        </View>
+        {/* To */}
+        <View style={styles.option}>
+          <AppText style={{ flex: 1 }} font={AppFonts['Urbanist-Medium']} >To </AppText>
+          <View style={styles.option}>
+            <AppText font={AppFonts['Urbanist-Medium']} >16:00 </AppText>
+            <RightChevron width={15} height={15} />
+          </View>
+        </View>
 
         <View style={styles.option}>
-          <AppText font={AppFonts['Urbanist-Medium']} >Turn OFF my video </AppText>
+          <AppText style={{ flex: 1 }} font={AppFonts['Urbanist-Medium']} >Time zone </AppText>
+          <View style={styles.option}>
+            <AppText font={AppFonts['Urbanist-Medium']} >Lagos/Africa </AppText>
+            <RightChevron width={15} height={15} />
+          </View>
+        </View>
+
+        <View style={styles.option}>
+          <AppText style={{ flex: 1 }} font={AppFonts['Urbanist-Medium']} >Repeat </AppText>
+          <View style={styles.option}>
+            <AppText font={AppFonts['Urbanist-Medium']} >Never </AppText>
+            <RightChevron width={15} height={15} />
+          </View>
+        </View>
+
+        <Divider />
+
+        <AppText font={AppFonts['Urbanist-Medium']} >Meeting Options</AppText>
+
+        <View style={styles.option}>
+          <AppText font={AppFonts['Urbanist-Medium']} >Enable waiting room </AppText>
           <Switch />
         </View>
         <View style={styles.option}>
-          <AppText font={AppFonts['Urbanist-Medium']} >Turn OFF my audio </AppText>
+          <AppText font={AppFonts['Urbanist-Medium']} >Add to calendar </AppText>
           <Switch />
         </View>
       </View>
@@ -68,7 +108,8 @@ const styles = StyleSheet.create({
   option: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    alignItems: 'center'
+    alignItems: 'center',
+    columnGap: 10
   }
 })
 
